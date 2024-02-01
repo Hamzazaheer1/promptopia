63a1d383e1951d587a37b1a710535ecfc93ffd47
Hamza Zaheer <mhamzazaheer1@gmail.com>
2024-02-01 23:22:41 +0500

    first commit

---
+++ app/layout.jsx
@@ -0,0 +1,29 @@
+import Nav from "@components/Nav";
+import Provider from "@components/Provider";
+import "@styles/global.css";
+
+export const metaData = {
+  title: "Promptopia",
+  description: "Discover & Share AI Prompts",
+};
+
+const RootLayout = ({ children }) => {
+  return (
+    <html lang="en">
+      <body>
+        <Provider>
+          <div className="main">
+            <div className="gradiant" />
+          </div>
+
+          <main className="app">
+            <Nav />
+            {children}
+          </main>
+        </Provider>
+      </body>
+    </html>
+  );
+};
+
+export default RootLayout;
